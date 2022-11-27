fa3875fcf045064f0246c7994b9415f94dcca852
amaralc <calil.amaral@dynamox.net>
2022-11-27 08:43:15 -0300

    refactor: rename projects

--- apps/shell_4200/jest.config.ts
+++ apps/shell_4200/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'shell',
+  displayName: 'shell_4200',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/shell',
+  coverageDirectory: '../../coverage/apps/shell_4200',
 };
